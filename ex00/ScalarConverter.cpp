b41a5da5c40bac4779b247bd81600d201ee63e61
root <sushi.honda@gmail.com>
2025-06-06 14:31:44 +0800

    ex00) feat: prototype

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 13:46:34 by athonda           #+#    #+#             */
+/*   Updated: 2025/06/06 14:11:29 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ScalarConverter.hpp"
+
+void	ScalarConverter::convert(std::string const &input)
+{
+	std::cout << input << std::endl;
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 13:33:08 by athonda           #+#    #+#             */
+/*   Updated: 2025/06/06 14:11:17 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class ScalarConverter
+{
+	public:
+		static void	convert(std::string const &input);
+	private:
+};
