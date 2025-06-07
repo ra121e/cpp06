8ea6920cd049da56d879ec7deb82eb6da0c9461f
root <sushi.honda@gmail.com>
2025-06-07 08:29:09 +0800

    ex00) feat: constructors in private
      to prohibit initialization
      no need destructor because no instatiation

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,15 +6,22 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:33:08 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/06 14:11:17 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/07 08:28:18 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 #include <string>
 
 class ScalarConverter
 {
 	public:
 		static void	convert(std::string const &input);
+
 	private:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter const &other);
+		ScalarConverter	&operator=(ScalarConverter const &other);
+
+		int	checkType(std::string const &s);
 };
