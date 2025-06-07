d7f75d4761e32e7e96b598dd53703884e786a310
root <sushi.honda@gmail.com>
2025-06-07 14:48:11 +0800

    ex00) feat: switch case from enum scalar type
      enum and switch is very familiar

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:46:34 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/07 14:35:43 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/07 14:47:50 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,25 @@ e_ScalarType		ScalarConverter::checkType(std::string const &s)
 void	ScalarConverter::convert(std::string const &input)
 {
 	std::cout << input << std::endl;
-	std::cout << checkType(input) << std::endl;
+	e_ScalarType	type = checkType(input);
+
+	switch(type)
+	{
+		case 0:
+			std::cout << type << std::endl;
+			break ;
+		case 1:
+			std::cout << type << std::endl;
+			break ;
+		case 2:
+			std::cout << type << std::endl;
+			break ;
+		case 3:
+			std::cout << type << std::endl;
+			break ;
+		case 4:
+			std::cout << type << std::endl;
+			break ;
+	}
 
 }
