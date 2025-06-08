b582d9a45f2c8d5a3d075e9881951769b62e344e
root <sushi.honda@gmail.com>
2025-06-08 13:16:49 +0800

    ex00) challenge: istream_iterator for std::cin

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,13 +6,14 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:46:34 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/08 12:11:59 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/08 13:16:19 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cctype>		// isdigit
 #include <string>		// string
 #include <iostream>		// std::cout
+#include <iterator>		// istream_iterator
 #include "ScalarConverter.hpp"
 
 int		ScalarConverter::checkChar(std::string const &s)
@@ -58,10 +59,22 @@ void	ScalarConverter::convert(std::string const &input)
 	std::cout << input << std::endl;
 	e_ScalarType	type = checkType(input);
 
-	// test for iterator
-	std::streambuf *buf = std::cin.rdbuf();
-	int c = buf->sbumpc();
-	std::cout << "the ascii number of input is: " << c << std::endl;
+	//test for iterator
+	std::istream_iterator<char>	a(std::cin);
+	std::istream_iterator<char>	b;
+
+	std::cout << "iterator a is " << *a << std::endl;
+	++a;
+	std::cout << "iterator a after increment is " << *a << std::endl;
+	std::cout << "iterator b as end is " << *b << std::endl;
+
+	// test for inside of std::cin
+	//std::streambuf *buf = std::cin.rdbuf();
+	//int c = buf->sbumpc();
+	//std::cout << "the ascii number of input is: " << c << std::endl;
+
+
+	//normal >>
 	//std::string str;
 	//std::cin >> str;
 	//std::cout << "cin test: str " << str << std::endl;
