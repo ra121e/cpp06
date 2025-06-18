feaab173e017a5d030832821d94ba096bef59c78
root <sushi.honda@gmail.com>
2025-06-18 12:53:04 +0800

    ex01) tests: clean up test cases and display messages

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 20:04:19 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/17 21:33:21 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/18 12:52:23 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,26 @@
 #include "Data.hpp"
 #include "Serializer.hpp"
 
+void	printBanner(std::string title)
+{
+	std::cout << "\n--- " << title << " ---" << std::endl;
+}
+
 int	main(void)
 {
 	Data	a;
-	std::cout << "this is the members of a" << std::endl;
+	printBanner("Simple test for address of struct.");
+	std::cout << "Here are the members of struct a" << std::endl;
 	std::cout << "char c: " << a.c << std::endl;
 	std::cout << "string: " << a.str << std::endl;
 	std::cout << "int num: " << a.num << std::endl;
-	std::cout << "address of a: " << &a << std::endl;
+	std::cout << "address of struct a: " << &a << std::endl;
 
 	uintptr_t	b;
-	std::cout << "serialize address of a to number" << std::endl;
+	std::cout << "serialize address type to number type" << std::endl;
 	b = Serializer::serialize(&a);
-	std::cout << "Serialized address a: " << b << std::endl;
+	std::cout << "Serialized value of the ex-address value of struct a: " << b << std::endl;
+	std::cout << "its same bit patern, but the translated meaning is different." << std::endl;
 
 	Data	*c;
 	std::cout << "Now, desirialize the number b to address" << std::endl;
@@ -37,24 +44,30 @@ int	main(void)
 	std::cout << "char c: " << c->c << std::endl;
 
 	// extra test for cast
-	Data	s;
 	Data_s	*ss;
+	printBanner("extra test for casting the large struct to small struct.");
 	std::cout << "Now, this is extra test for reinterpret_cast" << std::endl;
 	std::cout << "cast from Data class which has 3 members to Data_s class which has 2 members" << std::endl;
-	ss = reinterpret_cast<Data_s*>(&s);
+	ss = reinterpret_cast<Data_s*>(c);
 
-	std::cout << "short struct address: " << ss << std::endl;
-	std::cout << "char of ss " << ss->c << std::endl;
-	std::cout << "string of ss " << ss->str << std::endl;
-	std::cout << "we can call only 2 members for compiler restrict" << std::endl;
+	std::cout << "the small struct address: " << ss << std::endl;
+	std::cout << "char in the small struct " << ss->c << std::endl;
+	std::cout << "string in the small struct " << ss->str << std::endl;
+	std::cout << "we can call only 2 members because compiler restrict" << std::endl;
+	std::cout << "Trying to access to the third member makes compile error" << std::endl;
+
+	// compile error
+	// std::cout << ss.num << std::endl;
 
 	Data_s	t;
 	Data	*u;
 
 	u = reinterpret_cast<Data*>(&t);
+	printBanner("extra test for casting from smalll struct to large struct");
 	std::cout << "if small class casted to large class, we can access to the outside of class" << std::endl;
 	std::cout << "first line: " << u->c << std::endl;
 	std::cout << "second line: " << u->str << std::endl;
 	std::cout << "third line: " << u->num << std::endl;
+	std::cout << "This is the risk of reinterpret_cast" << std::endl;
 	return (0);
 }
